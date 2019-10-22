6d1a2d316ebd1995a712e6400f1dda000f7d74d0
mikeygbd <mikey.soares@gmail.com>
2019-10-22 09:38:46 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
 
 const app = express()
+
+app.use(authRoutes)
+
+const mongoUri = 'mongodb+srv://admin:jingel56@cluster0-czcf3.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance')
+})
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo')
+})
 
 app.get('/', (req, res) => {
   res.send('Hi There')
 })
 
 app.listen(3000, () => {
-  console.log('listinin on 3000')
+  console.log('listening on 3000')
 })
