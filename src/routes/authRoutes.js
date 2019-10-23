8befaef852966f2f5d73f4464b64a1b62f980127
mikeygbd <mikey.soares@gmail.com>
2019-10-22 23:05:53 -0400

    fixed typo of req.body

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -21,7 +21,7 @@ router.post('/signup', async (req, res) => {
 })
 
 router.post('/signin', async (req, res) => {
-  const { email, password } = req.bodyParser
+  const { email, password } = req.body
 
   if(!email || !password) {
     return res.status(422).send({ error: 'Must Provide email and password'})
