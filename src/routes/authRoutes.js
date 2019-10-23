c87970cde1b66958e0d7e55b76fcff93def4949e
mikeygbd <mikey.soares@gmail.com>
2019-10-22 21:13:34 -0400

    changed router.post to async. set email and password to req.body. set const user to a new User instance saved user. wrapped it all in a ty and catch

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,9 +4,18 @@ const User = mongoose.model('User')
 
 const router = express.Router()
 
-router.post('/signup', (req, res) => {
-  console.log(req.body)
+router.post('/signup', async (req, res) => {
+  const {email, password}  = req.body
+
+  try {
+  const user = new User({ email, password })
+
+  await user.save()
+
   res.send('You made a post request')
+} catch (err) {
+  return res.status(422).send(err.message)
+}
 })
 
 module.exports = router
